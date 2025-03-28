8bda461a425f1e3b5b3ff54246cb9bae38fd24e4
dcarvalho78 <danil.carvalho@dci-student.org>
2025-03-28 13:52:23 +0100

    upload productclass

--- inventory/product.py
+++ inventory/product.py
@@ -1,1 +1,14 @@
-test
+class Product:
+    def __init__(self, name, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def update_quantity(self, quantity):
+        self.quantity = quantity
+
+    def update_price(self, price):
+        self.price = price
+
+    def get_product_info(self):
+        return f"Product: {self.name}, Price: {self.price}, Quantity: {self.quantity}"
